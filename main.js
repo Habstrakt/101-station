f9626d28659bbde118e578e607183f72571ccf7c
Pavel <hunter3000@mail.ru>
2025-01-09 19:14:02 +1000

    add favicon and change social-link image

---
+++ main.js
@@ -0,0 +1,7 @@
+// const radioLinks = document.querySelectorAll(".animated-link");
+
+// radioLinks.forEach(link => {
+// 	link.addEventListener("click", function removeLink(e) {
+// 		e.preventDefault();
+// 	});
+// });
